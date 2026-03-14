2dad22b56485cbc42edc37359a8336bf75b40441
Marissa <mperry.5262@gmail.com>
2026-03-14 08:57:55 -0500

    Fixed BPM and AR-ANGLE extension in .fits hdul

--- wrappers/run_nirwals_workflow.py
+++ wrappers/run_nirwals_workflow.py
@@ -3,34 +3,71 @@
 import os
 import json
 import importlib
+from astropy.io import fits
+import glob
 
 def load_json(path):
     with open(path) as f:
         return json.load(f)
     
 
-# def fix_header_keywords(obs_date):
-#     '''
-#     nirwalsreduce.py expects header "AR-ANGLE" that does not exist in our raw data files. 
-#     Manually replace with header "CAMANG", which provides the same information ? 
-#     '''
+def fix_primary_header_keys(work_dir):
+    '''
+    nirwalsreduce.py expects header "AR-ANGLE" that does not exist in our raw data files. 
+    Manually replace with header "CAMANG", which provides the same information ? 
+    '''
 
-#     product_dir = os.path.join('..',obs_date, 'nirwals', 'product')
-#     product_files = glob.glob(os.path.join(product_dir, '*.fits'))
+    product_dir = os.path.join(work_dir, 'nirwals', 'product')
+    product_files = glob.glob(os.path.join(product_dir, '*.fits'))
     
-#     for product_file in product_files:
-#         basename = os.path.basename(product_file)
+    for product_file in product_files:
+        basename = os.path.basename(product_file)
         
-#         try:
-#             with fits.open(product_file, mode='update') as hdu:
-#                 header = hdu[0].header
+        try:
+            with fits.open(product_file, mode='update') as hdul:
+                header = hdul['PRIMARY'].header
+                header_keys = list(header.keys())
                 
-#                 # add AR-ANGLE if missing (use CAMANG value)
-#                 if 'AR-ANGLE' not in header and 'CAMANG' in header:
-#                     header['AR-ANGLE'] = (header['CAMANG'], '(manually added as a copy of CAMANG) Articulation angle [degrees]')
+                # add AR-ANGLE if missing (use CAMANG value)
+                if 'AR-ANGLE' not in header_keys and 'CAMANG' in header_keys:
+                    header['AR-ANGLE'] = (header['CAMANG'], 'Articulation angle [degrees] (copied from CAMANG)')
 
-#         except Exception as e:
-#             print(f"Error processing {basename}: {e}")
+        except Exception as e:
+            print(f"Error processing {basename}: {e}")
+
+# created this, but still need to implement it into main function !!!!!
+def add_bpm_header(work_dir):
+    '''
+     nirwalsreduce.py expects header "BPM" that does not exist in our raw data files. 
+     Manually create header using the bpm folder in the config directory
+     '''
+
+    # get BPM data from fits file
+    bpm_dir = os.path.join('nirwals_pipeline','salt_pkgs', 'configs', 'nirwals', 'bpm')
+    bpm_file = glob.glob(os.path.join(bpm_dir, 'NIRWALSBpm*.fits'))[0]
+    with fits.open(bpm_file) as hdul:
+        bpm_arr = hdul[1].data
+
+    # loop through raw data files
+    product_dir = os.path.join(work_dir, 'nirwals', 'product')
+    product_files = glob.glob(os.path.join(product_dir, '*.fits'))
+    for product_file in product_files:
+        basename = os.path.basename(product_file)
+        
+        try:
+            # add "BPM" hdu
+            with fits.open(product_file, mode='update') as hdul:
+                hdu_names = [hdu.name for hdu in hdul]   # list of hdu extension names
+                if 'BPM' in hdu_names:
+                    continue  # if this product file doesn't have it, check others
+                else:
+                    # create "BPM" hdu
+                    bpm_hdu = fits.ImageHDU(data=bpm_arr, name='BPM')
+                    bpm_hdu.header['COMMENT'] = 'bad-pixel mask'
+                    hdul.append(bpm_hdu) # add to hdu list
+
+        except Exception as e:
+            print(f"Error processing {basename}: {e}")
 
 
 def run_workflow(workflow_file, obs_date, param_dir, config_dir, work_dir, 
@@ -90,8 +127,9 @@ def run_workflow(workflow_file, obs_date, param_dir, config_dir, work_dir,
                 module_path = "nirwals_pipeline.salt_pkgs.code.saltreduce.saltreduce.science.nirwals.nirwalsreduce"
                 func_name = "reduce_data"
 
-                # fix_header_keywords(obs_date)  # replace "CAMANG" hdu header with "AR-ANGLE" (pipeline expects this for camera ange)
-            
+                fix_primary_header_keys(work_dir)  # replace "CAMANG" hdu header with "AR-ANGLE" (pipeline expects this for camera ange)
+                add_bpm_header(work_dir)  # add in 'BPM' hdu extension to product file using fits file in config directory
+
             else:
                 raise ImportError(f"Unknown pipeline module: {task["name"]}")
             
